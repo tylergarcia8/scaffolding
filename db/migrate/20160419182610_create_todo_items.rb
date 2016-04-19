ffcad2637236e606eba6dfb5376830fc3194314b
Tyler Garcia <t-garcia15@hotmail.com>
2016-04-19 15:12:54 -0400

    fix

--- db/migrate/20160419182610_create_todo_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodoItems < ActiveRecord::Migration
-  def change
-    create_table :todo_items do |t|
-      t.string :description
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160419182610) do
+ActiveRecord::Schema.define(version: 20160419181133) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "description"
